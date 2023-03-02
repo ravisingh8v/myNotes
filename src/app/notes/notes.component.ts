aa402d5f213bd2c1af8bce375cad018b45d4152d
Ravi Singh <ravi.singh@1rivet.local>
2023-03-02 18:58:04 +0530

    feat:working on note UI

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.scss']
+  styleUrls: ['./notes.component.scss'],
 })
 export class NotesComponent {
 
